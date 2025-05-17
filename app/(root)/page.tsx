39b9cb2f7127e91a892bfadbbf92a454a01ffb2a
Sandeep <sandeepdewangan7@gmail.com>
2025-05-17 13:43:05 +0530

    Active Links

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,7 @@
-import Link from "next/link";
 import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      Hello NextJS!
-      <Link href="/blog">Blog</Link>
-    </div>
-  );
+  return <div>Hello NextJS!</div>;
 };
 
 export default Home;
