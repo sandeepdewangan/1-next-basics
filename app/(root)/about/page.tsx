f11d9dd9cd4eceee9cdefbb819f3337a40a0caba
Sandeep <sandeepdewangan7@gmail.com>
2025-05-17 10:47:54 +0530

    Metadata Basics

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from "next";
 import React from "react";
 
+export const metadata: Metadata = {
+  title: "About Page",
+  description: "Desc",
+};
 const About = () => {
   return <div>About</div>;
 };
