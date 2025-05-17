2e10a9e2d693710d376860252184b13435f3453c
Sandeep <sandeepdewangan7@gmail.com>
2025-05-17 13:26:08 +0530

    Links

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const Home = () => {
-  return <div>Hello NextJS!</div>;
+  return (
+    <div>
+      Hello NextJS!
+      <Link href="/blog">Blog</Link>
+    </div>
+  );
 };
 
 export default Home;
