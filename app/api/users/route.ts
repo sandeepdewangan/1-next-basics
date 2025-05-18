67796d916ae283455c89eb0a49801e2f07494d14
Sandeep <sandeepdewangan7@gmail.com>
2025-05-18 13:29:06 +0530

    GET and POST API Requests

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,5 +1,18 @@
 import { users } from "@/app/usersData";
 
+// GET Request
 export async function GET() {
     return Response.json(users);
+}
+
+export async function POST(request: Request) {
+    const user = await request.json();
+
+    const newUser = {
+        id: users.length + 1,
+        name: user.name
+    }
+    users.push(newUser);
+
+    return new Response(JSON.stringify(newUser), { headers: { "Content-Type": "application/json" }, status: 201 });
 }
--- app/usersData.ts
+++ app/usersData.ts
@@ -2,16 +2,13 @@ export const users = [
     {
         id: 1,
         name: "sandeep",
-        age: 35,
     },
     {
         id: 2,
         name: "khushbu",
-        age: 29,
     },
     {
         id: 3,
         name: "cherry",
-        age: 3,
     },
 ];
