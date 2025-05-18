86ae9822deab2bc2a767b81a17f9ac87d6ad22f0
Sandeep <sandeepdewangan7@gmail.com>
2025-05-18 18:12:17 +0530

    Cookie in Route Handlers

--- app/api/users/profile/route.ts
+++ app/api/users/profile/route.ts
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
@@ -6,15 +6,25 @@ export async function GET(request: NextRequest) {
     const requestHeader = new Headers(request.headers);
     console.log(requestHeader.get("Authorization"));
 
-    // approach 2
+    // approach 2, next way
     const headersList = await headers();
     console.log(headersList.get("Authorization"));
+
+    // get cookie
+    const theme = request.cookies.get("theme");
+    console.log(theme);
+
+    // next way
+    const cookie = await cookies();
+    cookie.set("cartItemCount", "5");
+    console.log(cookie.get("cartItemCount"));
 
 
     // return new Response("Profile Data"); // returns simple plain/text response
     return new Response("Profile Data", {
         headers: {
             "Content-Type": "application/json",
+            "Set-Cookie": "theme=dark",
         }
     });
 }
