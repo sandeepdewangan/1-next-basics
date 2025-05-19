5235ba7493a8e62508b50141f06aae7cb194a204
Sandeep <sandeepdewangan7@gmail.com>
2025-05-19 21:56:13 +0530

    -

--- app/api/time/route.ts
+++
@@ -1,8 +0,0 @@
-// time is cached and served instantly to the users.
-// there is no caching in development.
-export const dynamic = "force-static";
-export const revalidate = 10; // refreshes the data every 10sec
-
-export async function GET() {
-    return Response.json({ time: new Date().toLocaleTimeString() });
-}
